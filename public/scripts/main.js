218c3cb555a52e8a21f2f730a35a86f0a26510ce
destradax <destradax@gmail.com>
2016-01-11 12:47:15 -0500

    Navbar links now work propperly.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,58 +1,105 @@
 'use strict';
+var nav = document.querySelector('nav');
 var navOpen = document.querySelector('#nav-open');
 var navClose = document.querySelector('#nav-close');
 var overlay = document.querySelector('#overlay');
-var primaryNavLinks = Array.prototype.slice.call(document.querySelectorAll('nav ul li a'));
+var primaryNavs = Array.prototype.slice.call(document.querySelectorAll('ul.primary > li'));
+var navLinks = Array.prototype.slice.call(document.querySelectorAll('nav ul li a'));
+
+/**
+ * Deactivates all the primary nav links.
+ */
+var deactivatePrimaryNavs = function () {
+	primaryNavs.forEach(function (nav) {
+		if (nav.classList.contains('active')) {
+			nav.classList.remove('active');
+		}
+	});
+};
+
+/**
+ * Shows the overlay
+ */
+var showOverlay = function () {
+	overlay.style.display = 'block';
+};
+
+/**
+ * Hides the overlay
+ */
+var hideOverlay = function () {
+	overlay.style.display = 'none';
+};
 
 /**
  * Opens the navigation bar.
  */
 var openNavbar = function () {
-	var nav = document.querySelector('nav');
 	if (nav.classList) {
 		nav.classList.add('active');
 	} else {
 		nav.className += 'active';
 	}
+	showOverlay();
 };
-navOpen.addEventListener('click', openNavbar);
 
 /**
- * Closes the navigation bar.
+ * Closes the navigation bar and hides the overlay.
  */
 var closeNavbar = function () {
-	var nav = document.querySelector('nav');
 	if (nav.classList && nav.classList.contains('active')) {
 		nav.classList.remove('active');
 	}
+	hideOverlay();
 };
-navClose.addEventListener('click', closeNavbar);
-overlay.addEventListener('click', closeNavbar);
 
 /**
- * Handles the click in the navbar items:
- * Remove the active class from other navbar items.
- * If item is a dropdown button, prevent navigation and instead open the dropdown content
- * If item is not a dropdown button, let navigation procceed.
+ * Reverts the navbar to its initial state.
+ */
+var resetNav = function () {
+	deactivatePrimaryNavs();
+	closeNavbar();
+};
+
+/**
+ * Handles the click in the navbar links:
+ * If it is a secondary nav, simply reset the nav and allow navigation.
+ * If it is a primary nav, and item is a dropdown button, prevent navigation and instead open the dropdown content.
+ * If it is a primary nav, and item is not a dropdown button, reset the nav and let navigation procceed.
  * @param {object} event The event that represents the click.
  */
 var clickNavLink = function (event) {
-	var li = this.parentNode;
-	var isActive = li.classList.contains('active');
-	var siblings = Array.prototype.slice.call(li.parentNode.children);
-	siblings.forEach(function (dropdown) {
-		if (dropdown.classList.contains('active')) {
-			dropdown.classList.remove('active');
-		}
-	});
+	var parentLi = this.parentNode;
+	var isPrimaryNavLink = parentLi.parentNode.classList.contains('primary');
 
-	if (this.classList.contains('dropdown-btn')) {
-		event.preventDefault();
-		if (!isActive) {
-			li.classList.add('active');
+	if (isPrimaryNavLink) {
+		var isActive = parentLi.classList.contains('active');
+
+		// If item is a dropdown button, prevent navigation and instead open the dropdown content.
+		if (this.classList.contains('dropdown-btn')) {
+			showOverlay();
+			deactivatePrimaryNavs();
+			event.preventDefault();
+			if (!isActive) {
+				parentLi.classList.add('active');
+			}
+		} else {
+			resetNav();
 		}
+	} else {
+		resetNav();
 	}
 };
-primaryNavLinks.forEach(function (navLink) {
-	navLink.addEventListener('click', clickNavLink);
-});
+
+/**
+ * Attaches the necessary events to their respective nodes.
+ */
+var initializeListeners = function () {
+	navOpen.addEventListener('click', openNavbar);
+	navClose.addEventListener('click', closeNavbar);
+	overlay.addEventListener('click', closeNavbar);
+	navLinks.forEach(function (navLink) {
+		navLink.addEventListener('click', clickNavLink);
+	});
+};
+initializeListeners();
