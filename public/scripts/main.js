9cfd46a64daf0828afe5ebbd34b1949dd6f1293a
destradax <destradax@gmail.com>
2016-01-10 21:16:28 -0500

    Primary navigation dropdown items now expand on click, not on hover.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,7 @@
 var navOpen = document.querySelector('#nav-open');
 var navClose = document.querySelector('#nav-close');
 var overlay = document.querySelector('#overlay');
+var primaryNavLinks = Array.prototype.slice.call(document.querySelectorAll('nav ul li a'));
 
 /**
  * Opens the navigation bar.
@@ -27,3 +28,31 @@ var closeNavbar = function () {
 };
 navClose.addEventListener('click', closeNavbar);
 overlay.addEventListener('click', closeNavbar);
+
+/**
+ * Handles the click in the navbar items:
+ * Remove the active class from other navbar items.
+ * If item is a dropdown button, prevent navigation and instead open the dropdown content
+ * If item is not a dropdown button, let navigation procceed.
+ * @param {object} event The event that represents the click.
+ */
+var clickNavLink = function (event) {
+	var li = this.parentNode;
+	var isActive = li.classList.contains('active');
+	var siblings = Array.prototype.slice.call(li.parentNode.children);
+	siblings.forEach(function (dropdown) {
+		if (dropdown.classList.contains('active')) {
+			dropdown.classList.remove('active');
+		}
+	});
+
+	if (this.classList.contains('dropdown-btn')) {
+		event.preventDefault();
+		if (!isActive) {
+			li.classList.add('active');
+		}
+	}
+};
+primaryNavLinks.forEach(function (navLink) {
+	navLink.addEventListener('click', clickNavLink);
+});
